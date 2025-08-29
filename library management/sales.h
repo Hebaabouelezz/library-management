85f13ac34e81528f00cf644771ee57441364c011
Heba Abou El-EZZ <bebaadel656@gmail.com>
2025-08-29 05:11:29 -0700

    Update sales.h

--- library management/sales.h
+++ library management/sales.h
@@ -22,5 +22,20 @@ struct Customer
 
 };
 
+struct DummyDataBook
+{
+	string id;
+	string name;
+	string author;
+	string category;
+	int status = 0;
+	int quantity= 0;
+	int returnDate = 0;
+	double sellPrice = 0;
+	double borrowPriceByDay = 0;
+};
+
+void insertDummyBooksData(Inventory*myInventory);
+
 void proccessSales(int UserAccess,int salesStatus, Book* BookSale, int borrowPeriod, Inventory* myInventory, string id, string &name, string &nationalId, string &phone);
 void getCustomerData(string &name, string &nationalId, string &phone);
